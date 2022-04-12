197a602a29c2a944e5bb3683036b6849c3e2b555
francosilva25 <e.franco250299@gmail.com>
2022-04-12 11:22:03 -0500

    create principal components pages

--- client/src/pages/Pages.jsx
+++ client/src/pages/Pages.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from './products/Products'
+import Login from './auth/Login'
+import Register from './auth/Register'
+import Cart from './cart/Cart'
+import NotFound from './utils/NotFound/NotFound'
 
 const Pages = () => {
   return (
-    <div>Main pages component</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<Cart />} />
+        
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
