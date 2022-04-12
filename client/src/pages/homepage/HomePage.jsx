d78ffa9e7596e6e771e395807c39527342586093
francosilva25 <e.franco250299@gmail.com>
2022-04-12 17:03:25 -0500

    Create home page

--- client/src/pages/homepage/HomePage.jsx
+++ client/src/pages/homepage/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import HeroSection from '../../components/HeroSection'
+import Products from '../../components/Products/Products'
+import Footer from '../../components/Footer'
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
+      <Products />
+      <Footer />
     </div>
   )
 }
