9611f1cf135f784c64c2ec3ef6b63248adf0b1ac
francosilva25 <e.franco250299@gmail.com>
2022-04-12 17:03:19 -0500

    Create products section

---
+++ client/src/components/Products/Products.jsx
@@ -0,0 +1,28 @@
+import { Box, Container, Grid } from "@mui/material";
+import React from "react";
+import Product from "./Product";
+
+const Products = () => {
+  return (
+    <div>
+      <Box>
+
+      <Container>
+        <Grid container py={5} sx={{ justifyContent: 'center'}}>
+          <Grid item md={4}>
+            <Product />
+          </Grid>
+          <Grid item md={4}>
+            <Product />
+          </Grid>
+          <Grid item md={4}>
+            <Product />
+          </Grid>
+        </Grid>
+      </Container>
+      </Box>
+    </div>
+  );
+};
+
+export default Products;
